ff7db565c797fb16d60ee8bf329580f7677ef26c
wutSalty <96165794+wutSalty@users.noreply.github.com>
2025-08-28 00:29:05 +1000

    basic working mockup of just one screen

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+const config = 
+{  
+  plugins: 
+  {    
+    "@tailwindcss/postcss": {},  
+  },
 };
 
 export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
-import Image from "next/image";
+
+const LineColour = "#029747";
+
+const LineCode = "T8";
+
+const FinalStop = "Revesby";
+
+const ViaStop = "Airport";
+
+const UpcomingStops = [
+  "Mascot",
+  "Domestic",
+  "International",
+  "Wolli Creek",
+  "Turella",
+  "Bardwell Park",
+  "Bexley North",
+  "Kingsgrove",
+  "Beverly Hills",
+  "Narwee",
+  "Riverwood",
+  "Padstow",
+  "Revesby",
+];
+
+const ScrollStyle = {
+  "--speed": `${UpcomingStops.length * 2}s`,
+  "--items": `${UpcomingStops.length}`,
+}
+
+function Upcoming({name}: {name: string}) {
+  return (
+    <>
+      <div className="w-[350] my-[24]">
+        <div className={`bg-white border-[3] border-[${LineColour}] aspect-square w-[28] mr-[12] rounded-full relative inline-block`}></div>
+        <p className="text-4xl inline-block">{name}</p>
+      </div>
+    </>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="aspect-video h-[480px] scale-100 bg-white border">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {/* clock */}
+        <div className="w-full h-[75] bg-[#425171] flex items-center justify-end">
+          <p className="text-2xl text-white mr-[24]">Time Now: 10:34</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        
+        <div className="flex h-[405]">
+          {/* left side */}
+          <div className="basis-1/2 h-[405]">
+            <div className="flex items-center">
+              <div className={`aspect-square w-[102] bg-[${LineColour}] mt-[16] ml-[64] mr-[16] rounded-[16] rounded-bl-[0] flex items-center justify-center`}>
+                <p className="text-6xl font-semibold text-white">{LineCode}</p>
+              </div>
+              <div className="mt-[16]">
+                <p className="text-4xl leading-12">to {FinalStop}<br/>via {ViaStop}</p>
+              </div>
+            </div>
+            <div className="flex h-[calc(405px-118px)]">
+              {/* Line and dot */}
+              <div className={`bg-[${LineColour}] relative w-[18] left-[64]`}>
+                <div className={`bg-white border-[6] border-[${LineColour}] aspect-square w-[40] rounded-full relative right-[11] top-[102]`}>
+                </div>
+              </div>
+
+              {/* Next stop: */}
+              <div className="relative h-[100] left-[90] top-[64]">
+                <p className="text-2xl">Next Stop</p>
+                <p className="text-5xl">Green Square</p>
+              </div>
+            </div>
+          </div>
+
+          {/* right side */}
+          <div className="basis-1/2">
+            <div className="flex relative h-[405px]">
+              {/* Line and dot */}
+              <div className={`bg-[${LineColour}] relative w-[12] left-[72]`}>
+              </div>
+
+              {/* Next stop: */}
+              <div className="relative h-[32] left-[90] top-[16]">
+                <p className="text-2xl">Stopping At</p>
+              </div>
+
+              <div className="absolute h-[calc(405px-64px)] top-[64] left-[64] overflow-y-hidden">
+                <div className="animate-scroll" style={ScrollStyle}>
+                  {UpcomingStops.map((s) => (
+                    <Upcoming key={s} name={s} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
