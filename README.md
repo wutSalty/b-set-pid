1b020bdafd949b1139f808b8ea23fb87951093cb
wutSalty <96165794+wutSalty@users.noreply.github.com>
2025-08-28 01:17:24 +1000

    squash tailwing bugs, add somewhat of a readme

--- README.md
+++ README.md
@@ -1,36 +1,12 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# What the heck is this
 
-## Getting Started
+B Sets (also known as Waratah Series 2) are double-decker electric multiple units that operate on the Sydney Trains network. Compared to their predecesors, B Sets were the first to include LCD passenger information displays (PIDs) for upcoming stop information. Since their introduction in 2018, the PIDs have only gone through one major refresh. However, compared to other rail networks, the information displayed on the B Sets PID is extremely basic and a poor use of otherwise valuable screen space.
 
-First, run the development server:
+This project is a work-in-progress somewhat functional mockup of what a dense and information rich display could look like.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## WIP
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- Create half-sized information screens
+- Create screens for arriving at a station
+- Add the ability to add custom routes
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,13 @@
+'use client'
+import { CSSProperties } from 'react';
+import { ImAirplane } from 'react-icons/im'
 
-const LineColour = "#029747";
-
-const LineCode = "T8";
-
-const FinalStop = "Revesby";
-
-const ViaStop = "Airport";
-
-const UpcomingStops = [
-  "Mascot",
-  "Domestic",
-  "International",
-  "Wolli Creek",
-  "Turella",
-  "Bardwell Park",
-  "Bexley North",
-  "Kingsgrove",
-  "Beverly Hills",
-  "Narwee",
-  "Riverwood",
-  "Padstow",
-  "Revesby",
-];
-
-const ScrollStyle = {
-  "--speed": `${UpcomingStops.length * 2}s`,
-  "--items": `${UpcomingStops.length}`,
-}
-
-function Upcoming({name}: {name: string}) {
+function Upcoming({name, colour}: {name: string, colour: string}) {
+  // TODO: Airplane icon for airport stations
   return (
     <>
       <div className="w-[350] my-[24]">
-        <div className={`bg-white border-[3] border-[${LineColour}] aspect-square w-[28] mr-[12] rounded-full relative inline-block`}></div>
+        <div className="bg-white border-[3] aspect-square w-[28] mr-[12] rounded-full relative inline-block" style={{borderColor: colour}}></div>
         <p className="text-4xl inline-block">{name}</p>
       </div>
     </>
@@ -40,12 +15,37 @@ function Upcoming({name}: {name: string}) {
 };
 
 export default function Home() {
+  const LineColour = "#029747";
+  const LineCode = "T8";
+  const FinalStop = "Revesby";
+  const ViaStop = "Airport";
+  const UpcomingStops = [
+    "Mascot",
+    "Domestic",
+    "International",
+    "Wolli Creek",
+    "Turella",
+    "Bardwell Park",
+    "Bexley North",
+    "Kingsgrove",
+    "Beverly Hills",
+    "Narwee",
+    "Riverwood",
+    "Padstow",
+    "Revesby",
+  ];
+
+  const ScrollStyle = {
+    "--speed": `${UpcomingStops.length * 2}s`,
+    "--items": `${UpcomingStops.length}`,
+  }
+
   return (
     <>
       <div className="aspect-video h-[480px] scale-100 bg-white border">
 
         {/* clock */}
-        <div className="w-full h-[75] bg-[#425171] flex items-center justify-end">
+        <div className="w-full h-[75] bg-[#354d5e] flex items-center justify-end">
           <p className="text-2xl text-white mr-[24]">Time Now: 10:34</p>
         </div>
         
@@ -53,22 +53,22 @@ export default function Home() {
           {/* left side */}
           <div className="basis-1/2 h-[405]">
             <div className="flex items-center">
-              <div className={`aspect-square w-[102] bg-[${LineColour}] mt-[16] ml-[64] mr-[16] rounded-[16] rounded-bl-[0] flex items-center justify-center`}>
+              <div className="aspect-square w-[102] mt-[16] ml-[32] mr-[16] rounded-[16] flex items-center justify-center" style={{backgroundColor: LineColour}}>
                 <p className="text-6xl font-semibold text-white">{LineCode}</p>
               </div>
               <div className="mt-[16]">
-                <p className="text-4xl leading-12">to {FinalStop}<br/>via {ViaStop}</p>
+                <p className="text-4xl leading-12">{FinalStop}<br/>via {ViaStop}</p>
               </div>
             </div>
             <div className="flex h-[calc(405px-118px)]">
               {/* Line and dot */}
-              <div className={`bg-[${LineColour}] relative w-[18] left-[64]`}>
-                <div className={`bg-white border-[6] border-[${LineColour}] aspect-square w-[40] rounded-full relative right-[11] top-[102]`}>
+              <div className="relative w-[18] left-[76]" style={{backgroundColor: LineColour}}>
+                <div className="bg-white border-[6] aspect-square w-[40] rounded-full relative right-[11] top-[102]" style={{borderColor: LineColour}}>
                 </div>
               </div>
 
               {/* Next stop: */}
-              <div className="relative h-[100] left-[90] top-[64]">
+              <div className="relative h-[100] left-[102] top-[64]">
                 <p className="text-2xl">Next Stop</p>
                 <p className="text-5xl">Green Square</p>
               </div>
@@ -79,7 +79,7 @@ export default function Home() {
           <div className="basis-1/2">
             <div className="flex relative h-[405px]">
               {/* Line and dot */}
-              <div className={`bg-[${LineColour}] relative w-[12] left-[72]`}>
+              <div className="relative w-[12] left-[72]" style={{backgroundColor: LineColour}}>
               </div>
 
               {/* Next stop: */}
@@ -88,9 +88,9 @@ export default function Home() {
               </div>
 
               <div className="absolute h-[calc(405px-64px)] top-[64] left-[64] overflow-y-hidden">
-                <div className="animate-scroll" style={ScrollStyle}>
+                <div className="animate-scroll" style={ScrollStyle as CSSProperties}>
                   {UpcomingStops.map((s) => (
-                    <Upcoming key={s} name={s} />
+                    <Upcoming key={s} name={s} colour={LineColour} />
                   ))}
                 </div>
               </div>
